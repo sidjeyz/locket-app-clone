bc6ecd4ac7e35664545ed5815958d5d216338c92
Гусейн Римиханов <gusejnrimihanov@192.168.1.4>
2024-10-12 16:05:26 +0300

    Initial commit

---
+++ Locket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Locket
+//
+//  Created by Сулейман on 10.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
