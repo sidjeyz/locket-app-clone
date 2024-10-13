51b4b9f0267df66dc1a0903935d2d08be9648593
Гусейн Римиханов <gusejnrimihanov@192.168.1.9>
2024-10-13 23:36:56 +0300

    lesson 2

---
+++ Locket/Fonts/Fonts.swift
@@ -0,0 +1,23 @@
+//
+//  Fonts.swift
+//  Locket
+//
+//  Created by Гусейн Римиханов on 13.10.2024.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+    
+    enum Caption2 {
+        
+        static var regular: UIFont {
+            return UIFont(name: "SF Pro Rounded", size: 11) ?? .systemFont(ofSize: 11)
+        }
+        
+        
+    }
+    
+    
+}
